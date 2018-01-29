61a052bbc4046161ce736e05f057c7ee79a01044
Tony <yhtcoding@gmail.com>
2018-01-29 15:34:52 -0800

    simplified

--- test.py
+++ test.py
@@ -1,39 +1,20 @@
 from neural_network import *
-import numpy as np
-from mnist import MNIST
+from data_prep import *
 
-def read_in_data():
-    mndata = MNIST('./mnist_data')
-    mndata.gz = True
-    images, labels = mndata.load_training()
-    return np.array(images[0:60000]).T, np.array(labels[0:60000])
-
-def normalize_data(images,labels):
-
-    new_images = images / 127.5 -1
-
-    new_labels = np.zeros((10,len(labels)))
-    def f(x):
-        new_labels[labels[x], x] = 1
-    list(map(f,[ i for i in range(len(labels))]))
-
-    return new_images,new_labels
 
 
 if __name__ == "__main__":
-    images, labels = read_in_data()
-    #check error
+
     net = Network(
-        layers = [784,50,50,10],
+        layers = [784,10],
         max_epoch = 200,
-        eta = 0.01,
+        eta = 0.0001,
         func = 'sigmoid',
         is_sqrt_initialize = True,
         is_shuffle = True,
-        is_momentum = True
+        momentum_type = 'momentum'
     )
-    images, labels = normalize_data(images, labels)
+    images,labels = data_prep()
     net.fit(images,labels)
-    # map(net.check_accuracy(images,labels)
 
 
