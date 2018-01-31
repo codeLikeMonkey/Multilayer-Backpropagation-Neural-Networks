bc4d836799c64bc6a91c234c21218ffcd94c54db
Tony <yhtcoding@gmail.com>
2018-01-30 21:41:59 -0800

    add test data

--- data_prep.py
+++ data_prep.py
@@ -1,11 +1,16 @@
 import numpy as np
 from mnist import MNIST
 
-def read_in_data():
+def read_in_data(s = "training"):
     mndata = MNIST('./mnist_data')
     mndata.gz = True
-    images, labels = mndata.load_training()
-    return np.array(images[0:60000]).T, np.array(labels[0:60000])
+    if s == "training":
+        images, labels = mndata.load_training()
+    if s == "test":
+        images, labels = mndata.load_training()
+
+    return np.array(images).T, np.array(labels)
+
 
 def normalize_data(images,labels):
 
@@ -20,7 +25,12 @@ def normalize_data(images,labels):
 
 
 def data_prep():
-    images, labels = read_in_data()
-    images, labels = normalize_data(images, labels)
+    training_images, training_labels = read_in_data(s = "training")
+    training_images, training_labels = normalize_data(training_images, training_labels)
+    test_images,test_labels = read_in_data(s = "test")
+    test_images,test_labels = normalize_data(test_images,test_labels)
 
-    return images,labels
+    return training_images,training_labels,test_images,test_labels
+
+if __name__ == "__main__":
+    data_prep()
